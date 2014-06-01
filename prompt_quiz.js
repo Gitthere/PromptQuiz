114f19d92d82d68e89a5e8a363043a9f0806e921
Gitthere <ichi4g@gmail.com>
2014-05-31 16:49:48 -1000

    added responses to all answers.  tried to fix score tabulation

--- prompt_quiz.js
+++ prompt_quiz.js
@@ -3,45 +3,50 @@
   var score=0;
 
   var answer = prompt("What is the capital of Hawaii?");
-    if (answer =="Honolulu") {
+    if (answer == "Honolulu") {
       var newscore = score+1;
+      alert("Correct!")
     } else {
-      newscore = score+0;
-    }
+      newscore = score+0; 
+      alert("Sorry, the answer is Honolulu.")
   }
 
   var answer = prompt("What is the state bird?");
     if (answer == "Nene") {
-        var newscore=score+1;
+        var newscore = newscore+1;
+        alert("That's right.")
     } else {
-      newscore = score+0;
-    }
+      newscore = score+0; 
+      alert("Nope. The answer is Nene.")  
   }
 
   var answer = prompt("What is the Hawaiian name for Diamond Head?");
     if (answer == "Leahi") {
-      var newscore = score+1;
+      var newscore = newscore+1;
+      alert("Awesome!")
     } else {
-      newscore = score+0;
-    }
+      newscore = score+0;  
+      alert("Nice try, but the correct answer is Leahi.")  
   }
 
   var answer = prompt("What is the traditional dance of Hawaii?");
     if (answer == "hula") {
-      var newscore = score+1;
+      var newscore = newscore+1;
+      alert("You are right again.")
     } else {
-        newscore = score+0;
-      }
+        newscore = score+0; 
+        alert("How could you not know that?")     
   }
 
   var answer = prompt("In what year did Hawaii become a state?");
     if (answer == "1959") {
-      var newscore = score+1;
+      var newscore = newscore+1;
+      alert("You got it, you braniac.")
     } else {
-          newscore = score+0;
-        }
+          newscore = score+0; 
+          alert("Not even close. The answer is 1959.  Please go buy a history book.")     
     }
-    =
+    
   alert ("Your score is " + (newscore) + "out of 5")
 
 })();
